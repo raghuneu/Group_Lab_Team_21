dd92c105641e4bd8bf6bce52a5581d8b36f71095
shriran <shrirangesh962@gmail.com>
2025-10-19 18:14:22 -0400

    3

--- Member2.java
+++ Member2.java
@@ -1,5 +1,5 @@
 public class Member2 {
-public static void main(String[] args) {
-System.out.println("Hello from Member 2!");
-}
+    public static void main(String[] args) {
+        System.out.println("Hello from Member 2!");
+    }
 }
